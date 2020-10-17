7d76e974cb5991349ae4fb731a5fd31d8d3bfcbe
Nilson Martins <nilsonmart@outlook.pt>
2020-10-17 22:38:57 +0100

    Adicionar arquivos de projeto.

---
+++ ApiRestNPF/Controllers/DivisibleController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiRestNPF.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DivisibleController : Controller
+    {
+        [HttpGet]
+        public IActionResult Calculates(long[] numbers)
+        {
+            foreach (var item in numbers)
+            {
+
+            }
+
+            return Ok();
+        }
+    }
+}
